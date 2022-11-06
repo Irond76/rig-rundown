66fdb158138a5f9bf0f8d9d3d04bff77c0ac2be4
Dale Bendixen <dalebendixen@gmail.com>
2022-11-05 21:24:21 -0400

    first commit added title and styles

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css';
+
+
+
+const Title = () => {
+  return (
+    <div className="main-title">
+        <h1>rig rundown</h1>
+        <hr />
+      </div>
+  )
+}
+
+export default Title
