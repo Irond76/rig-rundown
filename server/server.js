0bda6925659f6c783673657a9f97e21720c1f6c1
Dale Bendixen <dalebendixen@gmail.com>
2022-12-21 14:50:27 -0500

    started post route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT;
 
+app.post('/login', (req, res) => {
+    console.log('request hit your server');
+});
 
 app.listen(port, () => {
     console.log(`Rig Rundown Server Started On Port: ${port}`)
