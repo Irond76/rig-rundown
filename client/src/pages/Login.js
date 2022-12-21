b5ef583e315c3c9a618e948f73923bfca2390943
Dale Bendixen <dalebendixen@gmail.com>
2022-12-21 14:30:34 -0500

    added onChange to inputs and set the new state to value

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,19 +6,35 @@ const Login = () => {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
   const [loggedInUser, setLoggedInUser] = useState({
     userName: '',
-    password: ''
+    userPassword: ''
   });
 
-const {userName, password} = loggedInUser;
+const {userName, userPassword} = loggedInUser;
+const onChange = (e) => {
+  e.preventDefault();
+  setLoggedInUser({...loggedInUser, [e.target.name]: e.target.value})
+}
 
   return (
     <>
     {isUserLoggedIn ?
-    <form action="/login" method="POST" className='login-form'>
+    <form action="/login" method="POST" className='login-form' >
       <label htmlFor="userName">User Name: </label>
-      <input type="text" name="userName" id='userName'className='input' required/>
+      <input 
+      type="text" 
+      name="userName" 
+      value={userName} 
+      id='userName'
+      className='input' 
+      required 
+      onChange={(e) => onChange(e)}/>
       <label htmlFor="userPassword">Password: </label>
-      <input type="password" name='userPassword' id='userPassword'className='input' required/>
+      <input 
+      type="password" 
+      name='userPassword' 
+      value={userPassword} 
+      id='userPassword'className='input' 
+      required onChange={(e) => onChange(e)}/>
       <button type='submit'className='submit-btn'>Submit</button>
     </form>
     : 
