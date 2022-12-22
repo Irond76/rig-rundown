fff30efaba77c95fc0720b6f6bdcfaaf50b03a2b
Dale Bendixen <dalebendixen@gmail.com>
2022-12-21 19:46:58 -0500

    connected to data-base

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const dotenv = require('dotenv').config();
+const DB = process.env.MONGO_DB_URI;
+const mongoose = require('mongoose');
 const port = process.env.PORT;
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+mongoose.set('strictQuery', false);
 
 
 app.post('/login', (req, res) => {
@@ -17,6 +20,15 @@ app.post('/login', (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Rig Rundown Server Started On Port: ${port}`)
-});
+
+const startUp = async () => {
+    try {
+        mongoose.connect(DB);
+        app.listen(port, () => {
+            console.log(`Rig Rundown Server Started On Port: ${port}\nConnected to Rig Rundown Data-Base...`)
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+startUp();
