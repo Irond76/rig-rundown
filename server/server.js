b2b899442b972ea0b95cccfad7e21d10ff8d5a44
Dale Bendixen <dalebendixen@gmail.com>
2022-12-21 19:24:17 -0500

    added simple check for userName

--- server/server.js
+++ server/server.js
@@ -9,7 +9,12 @@ app.use(express.urlencoded({extended: true}))
 
 app.post('/login', (req, res) => {
     const data = req.body;
-    console.log(data)
+    if (data.userName === 'Dale'){
+        console.log(data);
+
+    }else {
+        console.log('user not found')
+    }
 });
 
 app.listen(port, () => {
