559cc346d9b07cce39f19d3844fe560c3419478d
Dale Bendixen <dalebendixen@gmail.com>
2023-01-12 13:29:19 -0500

    fixed spacing issue on title for large screens

--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
   return (
     <>
     <div className="main-title">
-        <h1>rig rundown</h1>
+        <h1 className='title'>rig rundown</h1>
         <hr />
         <h2 className='title-subheader'>Your Catalogue Of Music Gear</h2>
     </div>
