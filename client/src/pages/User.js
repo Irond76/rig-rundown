0718418adf223a71036a8894042806721ced657d
Dale Bendixen <dalebendixen@gmail.com>
2023-01-15 15:48:36 -0500

    fetched data from backend, displaying on user page

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -1,17 +1,48 @@
-import AddItemButton from "../components/AddItemButton"
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import AddItemButton from "../components/AddItemButton";
 
 
 const User = () => {
+  const [myGear, setMyGear] = useState([]);
+
+  const getGearData = async () => {
+    const res = await axios.get('/user');
+    const data = await res.data;
+    setMyGear(data)
+    console.log(data);
+  };
+  useEffect(() => {
+    getGearData();
+  }, [])
   return (
+    <>
     <div>
       <AddItemButton />
-      <br />
-      <br />
-      <h1>WELCOME LOGGED IN USER </h1>
-      <br />
-      <br />
-      <h1>To Your New Home Page</h1>
     </div>
+    <div>
+      <article>
+        <section>
+        {myGear.map((item) => {
+          const {_id, brand,color,details,image,model,year,serialNumber} = item;
+          return (
+            <ul>
+              <li key={_id}>
+                <h1>{brand}</h1>
+                <h2>{model}</h2>
+                <h2>{color}</h2>
+                <h3>{year}</h3>
+                <img src={image} alt={`${brand} ${model}`} />
+                <h3>Serial NUmber: {serialNumber}</h3>
+                <p>{details}</p>
+              </li>
+            </ul>
+          )
+        })}
+        </section>
+      </article>
+    </div>
+  </>
   )
 }
 export default User
