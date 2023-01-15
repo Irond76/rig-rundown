6870e079a75f89083c72d9057f4f197c4b475df2
Dale Bendixen <dalebendixen@gmail.com>
2023-01-15 18:54:22 -0500

    fixed model bug

--- server/models/AddGear.js
+++ server/models/AddGear.js
@@ -5,6 +5,7 @@ const gearSchema = new mongoose.Schema({
     model: {type: String, required:true},
     serialNumber: {type: String, required:true},
     year: {type: String, required:true},
+    type: {type: Object, required: true},
     color: {type: String, required:true},
     image: {type: String },
     details: {type: String, required:true}
