3d3585d27c6f1288cdd06fdebccd2147074838f5
Dale Bendixen <dalebendixen@gmail.com>
2023-01-18 20:23:32 -0500

    changed cors

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.API_SECRET,
 });
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(fileUpload());
